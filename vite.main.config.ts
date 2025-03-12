6c326cecdc5b76449bf6773a1a20ce016af323a8
trustieee <mariocatch@gmail.com>
2025-03-11 23:02:52 -0400

    updates

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,17 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    // Some libs that can run in both Web and Node.js need to be told how to resolve
+    conditions: ['node', 'require'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['electron'],
+    },
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+});
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    conditions: ['node', 'import']
+  },
+  build: {
+    rollupOptions: {
+      external: ['electron']
+    }
+  }
+});
